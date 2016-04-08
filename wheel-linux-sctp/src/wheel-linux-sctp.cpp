0ac0fd7512344995265db2a9408871693cc7eed0
Jake Zhang <jake.zhang@gecogaming.com>
2016-04-08 10:52:05 +1000

    Init

---
+++ wheel-linux-sctp/src/wheel-linux-sctp.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : wheel-linux-sctp.cpp
+// Author      : Jackie
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+    return 0;
+}
