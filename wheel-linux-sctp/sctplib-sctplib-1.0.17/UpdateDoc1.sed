de77e22bd469f5060d4378ea3198b2956fed55f5
Kiddinglife <jakezhang1989@hotmail.com>
2016-04-16 19:48:03 +1000

    UP

--- wheel-linux-sctp/sctplib-sctplib-1.0.17/UpdateDoc1.sed
+++
@@ -1,2 +0,0 @@
-s/2016 Thomas Dreibholz/2016 Thomas Dreibholz/g
-s/2016 by Thomas Dreibholz/2016 by Thomas Dreibholz/g
--- wheel-linux-sctp/sctplib-sctplib-1.0.17/autogen.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-./bootstrap && ./configure --enable-static --disable-shared --enable-maintainer-mode --enable-sctp-over-udp $@ && make
