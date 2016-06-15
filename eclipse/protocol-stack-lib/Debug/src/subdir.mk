321fd0197b865b613c8964edfe2494bc2ec18d60
kiddinglife <jakezhang1989@hotmail.com>
2016-06-15 19:58:53 +1000

    up

--- eclipse/protocol-stack-lib/Debug/src/subdir.mk
+++ eclipse/protocol-stack-lib/Debug/src/subdir.mk
@@ -3,22 +3,15 @@
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+/home/jakez/2016209/geco-sctp-cplus/src/geco-malloc.cpp 
+
 CC_SRCS += \
 /home/jakez/2016209/geco-sctp-cplus/src/auth.cc \
 /home/jakez/2016209/geco-sctp-cplus/src/dispatch_layer.cc \
 /home/jakez/2016209/geco-sctp-cplus/src/gecotimer.cc \
 /home/jakez/2016209/geco-sctp-cplus/src/globals.cc \
 /home/jakez/2016209/geco-sctp-cplus/src/transport_layer.cc 
-
-CPP_SRCS += \
-/home/jakez/2016209/geco-sctp-cplus/src/geco-malloc.cpp 
-
-CC_DEPS += \
-./src/auth.d \
-./src/dispatch_layer.d \
-./src/gecotimer.d \
-./src/globals.d \
-./src/transport_layer.d 
 
 OBJS += \
 ./src/auth.o \
@@ -28,6 +21,13 @@ OBJS += \
 ./src/globals.o \
 ./src/transport_layer.o 
 
+CC_DEPS += \
+./src/auth.d \
+./src/dispatch_layer.d \
+./src/gecotimer.d \
+./src/globals.d \
+./src/transport_layer.d 
+
 CPP_DEPS += \
 ./src/geco-malloc.d 
 
@@ -36,42 +36,42 @@ CPP_DEPS += \
 src/auth.o: /home/jakez/2016209/geco-sctp-cplus/src/auth.cc
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -std=c++0x -I"/home/jakez/2016209/geco-sctp-cplus/include" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -std=c++0x -I"/home/jakez/2016209/geco-sctp-cplus/include" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 src/dispatch_layer.o: /home/jakez/2016209/geco-sctp-cplus/src/dispatch_layer.cc
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -std=c++0x -I"/home/jakez/2016209/geco-sctp-cplus/include" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -std=c++0x -I"/home/jakez/2016209/geco-sctp-cplus/include" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 src/geco-malloc.o: /home/jakez/2016209/geco-sctp-cplus/src/geco-malloc.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -std=c++0x -I"/home/jakez/2016209/geco-sctp-cplus/include" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -std=c++0x -I"/home/jakez/2016209/geco-sctp-cplus/include" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 src/gecotimer.o: /home/jakez/2016209/geco-sctp-cplus/src/gecotimer.cc
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -std=c++0x -I"/home/jakez/2016209/geco-sctp-cplus/include" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -std=c++0x -I"/home/jakez/2016209/geco-sctp-cplus/include" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 src/globals.o: /home/jakez/2016209/geco-sctp-cplus/src/globals.cc
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -std=c++0x -I"/home/jakez/2016209/geco-sctp-cplus/include" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -std=c++0x -I"/home/jakez/2016209/geco-sctp-cplus/include" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 src/transport_layer.o: /home/jakez/2016209/geco-sctp-cplus/src/transport_layer.cc
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -std=c++0x -I"/home/jakez/2016209/geco-sctp-cplus/include" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -std=c++0x -I"/home/jakez/2016209/geco-sctp-cplus/include" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- eclipse/protocol-stack-uinttests/Debug/unittets/subdir.mk
+++ eclipse/protocol-stack-uinttests/Debug/unittets/subdir.mk
@@ -3,18 +3,18 @@
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
-CC_SRCS += \
-/home/jakez/2016209/geco-sctp-cplus/unittets/test-main.cc 
-
 CPP_SRCS += \
 /home/jakez/2016209/geco-sctp-cplus/unittets/unit_tests.cpp 
 
-CC_DEPS += \
-./unittets/test-main.d 
+CC_SRCS += \
+/home/jakez/2016209/geco-sctp-cplus/unittets/test-main.cc 
 
 OBJS += \
 ./unittets/test-main.o \
 ./unittets/unit_tests.o 
+
+CC_DEPS += \
+./unittets/test-main.d 
 
 CPP_DEPS += \
 ./unittets/unit_tests.d 
@@ -24,14 +24,14 @@ CPP_DEPS += \
 unittets/test-main.o: /home/jakez/2016209/geco-sctp-cplus/unittets/test-main.cc
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross G++ Compiler'
-	g++ -std=c++0x -I/home/jakez/2016209/geco-sctp-cplus/thirdparty/googletest/include -I/home/jakez/2016209/geco-sctp-cplus/thirdparty/googlemock/include -I/home/jakez/2016209/geco-sctp-cplus/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -std=c++0x -I/home/jakez/2016209/geco-sctp-cplus/thirdparty/googletest/include -I/home/jakez/2016209/geco-sctp-cplus/thirdparty/googlemock/include -I/home/jakez/2016209/geco-sctp-cplus/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
 unittets/unit_tests.o: /home/jakez/2016209/geco-sctp-cplus/unittets/unit_tests.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross G++ Compiler'
-	g++ -std=c++0x -I/home/jakez/2016209/geco-sctp-cplus/thirdparty/googletest/include -I/home/jakez/2016209/geco-sctp-cplus/thirdparty/googlemock/include -I/home/jakez/2016209/geco-sctp-cplus/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -std=c++0x -I/home/jakez/2016209/geco-sctp-cplus/thirdparty/googletest/include -I/home/jakez/2016209/geco-sctp-cplus/thirdparty/googlemock/include -I/home/jakez/2016209/geco-sctp-cplus/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
