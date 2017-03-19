fa579c5fda0fdc8a4c73efacf1f3349e11cc5e1f
kiddinglife <jakezhang1989@hotmail.com>
2017-03-19 15:46:35 +1100

    add test-mrecv.cc

---
+++ unittets/test-mrecv.cc
@@ -0,0 +1,46 @@
+/*
+ * test-mrecv.cc
+ *
+ *  Created on: Mar 19, 2017
+ *      Author: jakez
+ */
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+#include "geco-test.h"
+#include "geco-net-chunk.h"
+
+struct mrecv : public testing::Test
+{
+    recv_controller_t* mrecv_;
+    geco_channel_t* old_channel;
+
+    virtual void
+    SetUp ()
+    {
+      alloc_geco_channel ();
+      old_channel = curr_channel_;
+    }
+    virtual void
+    TearDown ()
+    {
+      free_geco_channel ();
+    }
+    void
+    reset ()
+    {
+      //reset everything of 'path' to its init valuess
+      curr_channel_ = old_channel;
+      curr_geco_instance_ = curr_channel_->geco_inst;
+    }
+};
+
+/// insert chunk_tsn in the list of duplicates from small to big if it is not in list
+/// @param chunk_tsn    tsn we just received
+extern void mrecv_update_duplicates(recv_controller_t* mrecv, uint chunk_tsn);
+TEST_F(mrecv, test_mrecv_update_duplicates)
+{
+
+}
+
